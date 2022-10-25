63e2aec5649c6b319eb1a6f7175ccb5f515cd690
faratim <104940465+faratim@users.noreply.github.com>
2022-10-25 19:24:15 -0400

    updated tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
+import './App.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
+        Testing
         <CitySearch />
-        <NumberOfEvents />
         <EventList />
+        <NumberOfEvents />
       </div>
     );
   }
--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -4,11 +4,9 @@ import EventList from '../EventList';
 import Event from '../Event';
 import { mockData } from '../mock-data';
 
-describe('<EventList> component', () => {
+describe('<EventList /> component', () => {
     test('render correct number of events', () => {
-        const EventListWrapper = shallow(
-            <EventList
-                events={mockData} />);
+        const EventListWrapper = shallow(<EventList events={mockData} />);
         expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
-    });
+      });
 });
