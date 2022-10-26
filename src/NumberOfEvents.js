3d9ad88db535a95dda7d0f3172c3bd4c70e9b044
faratim <104940465+faratim@users.noreply.github.com>
2022-10-26 15:59:46 -0400

    added change handler

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -5,8 +5,14 @@ class NumberOfEvents extends Component {
         numberOfEvents: 32,
     }
     
+    changeNumberOfEvents(value) {
+        this.setState({ numberOfEvents: value });
+    }
+
     render() {
         
+        const { numberOfEvents } = this.state;
+
         return (
             <div className="number-of-events">
                 <label> Number of Events
@@ -14,6 +20,9 @@ class NumberOfEvents extends Component {
                         type="number"
                         className="number-of-events-input"
                         value={this.state.numberOfEvents}
+                        onChange={(event) => {
+                            this.changeNumberOfEvents(event.target.value);
+                        }}
                     ></input>
                 </label>
                 
