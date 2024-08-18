d169f0a6a196edf76b78dce478f3382405644360
faratim <faratim@gmail.com>
2024-08-18 10:14:14 -0400

    replacing Google logo on Sign in

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -14,7 +14,7 @@ function WelcomeScreen(props) {
                 <div className='button_cont' align='center'>
                     <div class='google-btn'>
                         <div class='google-icon-wrapper'>
-                            <img class='google-icon' src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg' alt='Google sign-in' />
+                            <img class='google-icon' src='https://images.app.goo.gl/aWKMYmHA8WZGVzCh8' alt='Google sign-in' />
                         </div>
                         <button
                             onClick={() => {
